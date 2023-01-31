ac6114b22b609bb4f21fe9e1d6345575f73b732b
Aymurat <aymurat@icloud.com>
2023-01-31 19:04:24 +0300

    Initial Commit

---
+++ TrafficLight4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrafficLight4
+//
+//  Created by Aymurat on 31.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
